0ffcd07c64200653af063f6e352e36b103cc8105
KamiNation <israeladedamola47@gmail.com>
2024-08-14 16:03:40 +0100

    This files were not present earlier

---
+++ server/utils/sendMail.ts
@@ -0,0 +1,84 @@
+require("dotenv").config()
+import nodemailer, { Transporter } from "nodemailer"
+import ejs from "ejs"
+import path from "path"
+
+
+console.log("ACTIVATION_SECRET:", process.env.ACTIVATION_SECRET);
+
+
+interface EmailOptions {
+    email: string
+    subject: string
+    // htmls: string
+    template: string
+    data: { [key: string]: any }
+}
+
+const sendMail = async (options: EmailOptions) => {
+
+
+    try {
+
+        const transporter: Transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: parseInt(process.env.SMTP_PORT || '587'),
+            service: process.env.SERVICE,
+            auth: {
+                user: process.env.SMTP_EMAIL,
+                pass: process.env.SMTP_PASSWORD
+            }
+        });
+
+        const { email, subject, template, data } = options
+        // const { email, subject, htmls } = options
+
+        console.log("email in sendmail =>", email);
+        console.log("subject in sendmail =>", subject);
+        console.log("template in sendmail =>", template);
+        console.log("data in sendmail", data);
+        
+        
+        
+
+        // get the path to the email template
+        // await ejs.renderFile(path.join(__dirname, "../mails/activation-email.ejs"), data)
+        const templatePath = path.join(__dirname, "../mails/", template)
+        console.log("templatePath from registrationUser =>", templatePath);
+
+        // console.log("Current working directory:", process.cwd());
+
+        
+
+        // Render email template with ejs
+        let html: string;
+        try {
+            html = await ejs.renderFile(templatePath, data);
+            console.log("html in sendmail =>", html);
+            // html: htmls
+            // console.log("html in sendmail =>", htmls);
+
+        } catch (error: any) {
+            throw new Error(`Failed to render email template: ${error.message}`);
+        }
+
+
+        // Header to send to user email
+        const mailHeader = {
+            from: process.env.SMTP_EMAIL,
+            to: email,
+            subject,
+            html
+        }
+
+        // send the mail
+        await transporter.sendMail(mailHeader)
+    } catch (error: any) {
+
+        console.error(`Failed to send email: ${error.message}`);
+        throw new Error(`Email sending failed: ${error.message}`);
+    }
+};
+
+
+export default sendMail
